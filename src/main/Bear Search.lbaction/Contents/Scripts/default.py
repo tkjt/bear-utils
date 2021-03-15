4aefda13e49b6024f9ebdadc8ffeea6b5ffeb49f
Teemu Turpeinen <12680397+tkjt@users.noreply.github.com>
2021-03-15 09:21:46 +0200

    General re-factored Bear Search script to make SonarLint happy

--- src/main/Bear Search.lbaction/Contents/Scripts/default.py
+++ src/main/Bear Search.lbaction/Contents/Scripts/default.py
@@ -14,13 +14,55 @@ bear_base: str = os.path.join(HOME, 'Library/Group Containers/9K33E3U3T4.net.shi
 bear_db: str = os.path.join(bear_base, 'database.sqlite')
 
 
-def clear_filter(in_string: str, suffix: str) -> str:
-    in_string = in_string.strip()
+def clear_filter(filter_str: str, suffix: str) -> str:
+    filter_str = filter_str.strip()
 
-    if in_string.endswith(suffix):
-        in_string = in_string[:-(len(suffix))]
+    if filter_str.endswith(suffix):
+        filter_str = filter_str[:-(len(suffix))]
 
-    return in_string
+    return filter_str
+
+
+def construct_query(query_opt: list) -> dict:
+    in_limit: bool = False
+    title_limit: str = ""
+    text_limit: str = ""
+    query: dict = {}
+    keywords: list = ['AND', 'OR']
+    query_filter: str = "OR"
+
+    if query_opt[0] in keywords:
+        query_filter = query_opt.pop(0)
+
+    for i in query_opt:
+        if i in keywords:
+            if in_limit:
+                title_limit = clear_filter(title_limit, query_filter) + ") "
+                text_limit = clear_filter(text_limit, query_filter) + ") "
+
+            title_limit = clear_filter(title_limit, query_filter) + i + " ("
+            text_limit = clear_filter(text_limit, query_filter) + i + " ("
+
+            in_limit = True
+        elif i != "":
+            if in_limit:
+                title_limit += "ZTITLE like \'%" + i + "%\' " + query_filter + " "
+                text_limit += "ZTEXT like \'%" + i + "%\' " + query_filter + " "
+            else:
+                title_limit += "(ZTITLE like \'%" + i + "%\') " + query_filter + " "
+                text_limit += "(ZTEXT like \'%" + i + "%\') " + query_filter + " "
+
+    title_limit = clear_filter(title_limit, query_filter)
+    text_limit = clear_filter(text_limit, query_filter)
+
+    if in_limit:
+        title_limit += ")"
+        text_limit += ")"
+
+    query['text'] = text_limit
+    query['title'] = title_limit
+
+    return query
 
 
 def search():
@@ -28,57 +70,21 @@ def search():
     notes: list = []
 
     if query is not None and query != "":
-        query_base: str = "SELECT ZTITLE, ZUNIQUEIDENTIFIER FROM ZSFNOTE " \
-                     "WHERE ZTRASHED = 0 AND ZARCHIVED = 0 AND ZENCRYPTED = 0"
-
-        title_limit: str = ""
-        text_limit: str = ""
         query = query.strip()
 
         if len(query) < 3:
             return
+
+        query_base: str = "SELECT ZTITLE, ZUNIQUEIDENTIFIER FROM ZSFNOTE " \
+                          "WHERE ZTRASHED = 0 AND ZARCHIVED = 0 AND ZENCRYPTED = 0"
 
         if query.endswith("AND") or query.endswith("OR"):
             query = clear_filter(query, "AND")
             query = clear_filter(query, "OR")
 
         query_opt: list = query.split(" ")
-        query_filter: str = "OR"
-
-        if query_opt[0] == "AND" or query_opt[0] == "OR":
-            query_filter = query_opt.pop(0)
-
-        in_limit: bool = False
-
-        for i in query_opt:
-            if i == "":
-                continue
-
-            if i == "OR" or i == "AND":
-                if in_limit:
-                    title_limit = clear_filter(title_limit, query_filter) + ") "
-                    text_limit = clear_filter(text_limit, query_filter) + ") "
-
-                title_limit = clear_filter(title_limit, query_filter) + i + " ("
-                text_limit = clear_filter(text_limit, query_filter) + i + " ("
-
-                in_limit = True
-            else:
-                if in_limit:
-                    title_limit += "ZTITLE like \'%" + i + "%\' " + query_filter + " "
-                    text_limit += "ZTEXT like \'%" + i + "%\' " + query_filter + " "
-                else:
-                    title_limit += "(ZTITLE like \'%" + i + "%\') " + query_filter + " "
-                    text_limit += "(ZTEXT like \'%" + i + "%\') " + query_filter + " "
-
-        title_limit = clear_filter(title_limit, query_filter)
-        text_limit = clear_filter(text_limit, query_filter)
-
-        if in_limit:
-            title_limit += ")"
-            text_limit += ")"
-
-        query = query_base + " AND (" + title_limit.strip() + ") OR (" + text_limit.strip() + ")"
+        q: dict = construct_query(query_opt)
+        query = query_base + " AND ((" + str(q['title']).strip() + ") OR (" + str(q['text']).strip() + "))"
 
         with sqlite3.connect(bear_db) as conn:
             conn.row_factory = sqlite3.Row
